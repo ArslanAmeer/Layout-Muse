ea32d1774e16254cbe281c6f4bb65974eaac2f06
Arslan Ameer <arslan.ameer@hotmail.com>
2018-03-05 05:13:22 +0500

    Add Main Header
    Main header with icon and list items.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,11 +2,12 @@
 
 var search = document.getElementById("search");
 search.onfocus = function(){
-    this.parentNode.style.border = "1px solid white";
+    this.parentNode.style.boxShadow = "inset 0px 0px 2px 1px rgba(255,255,255,1)";
     this.removeAttribute("placeholder");
 }
 
 search.onblur = function(){
-    this.parentNode.style.border = "none";
+    this.parentNode.style.boxShadow = "none";
     this.placeholder = "Search";
 }
+
